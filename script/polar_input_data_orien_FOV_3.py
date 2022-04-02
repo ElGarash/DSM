3ae31143f3bca83d8601033ebffa4f052690c842
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2022-04-03 01:37:52 +0200

    Limit ground directories to 5 parts

--- script/polar_input_data_orien_FOV_3.py
+++ script/polar_input_data_orien_FOV_3.py
@@ -15,7 +15,7 @@ class InputData:
         aerial_files_path = []
         ground_files_path = []
 
-        for grd_part in grd_parts:
+        for grd_part in grd_parts[:5]:
             part_path = f'{grds_root_path}/{grd_part}/frames'
             for simple_dir in listdir(part_path): 
 
