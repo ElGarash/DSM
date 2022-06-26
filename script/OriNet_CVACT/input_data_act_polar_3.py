3b467a3a979b46a74283ab5d9732089c703fb17d
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2022-06-26 13:51:12 +0200

    Remove unused variables and memory allocations

--- script/OriNet_CVACT/input_data_act_polar_3.py
+++ script/OriNet_CVACT/input_data_act_polar_3.py
@@ -31,7 +31,6 @@ class InputData:
         self.allDataList = '/kaggle/input/cvact-small/ACT_data.mat'
         print('InputData::__init__: load %s' % self.allDataList)
 
-        self.__cur_allid = 0  # for training
         self.id_alllist = []
         self.id_idx_alllist = []
 
@@ -43,20 +42,13 @@ class InputData:
         for i in range(0, len(anuData['panoIds'])):
             grd_id_align = self.img_root_polar + 'streetview_polish/' + anuData['panoIds'][i] + '_grdView.jpg'
             polar_sat_id_ori = self.img_root_polar + 'polarmap/' + anuData['panoIds'][i] + '_satView_polish.jpg'
-            sat_id_ori = self.img_root + 'satview_polish/satview_polish/' + anuData['panoIds'][i] + '_satView_polish.jpg'
 
-            self.id_alllist.append([grd_id_align, sat_id_ori, anuData['utm'][i][0], anuData['utm'][i][1], polar_sat_id_ori])
+            self.id_alllist.append([grd_id_align, polar_sat_id_ori])
             self.id_idx_alllist.append(idx)
             idx += 1
         self.all_data_size = len(self.id_alllist)
         print('InputData::__init__: load', self.allDataList, ' data_size =', self.all_data_size)
 
-        # partion the images into cells
-
-        self.utms_all = np.zeros([2, self.all_data_size], dtype=np.float32)
-        for i in range(0, self.all_data_size):
-            self.utms_all[0, i] = self.id_alllist[i][2]
-            self.utms_all[1, i] = self.id_alllist[i][3]
 
         self.training_inds = anuData['trainSet']['trainInd'][0][0] - 1
 
@@ -64,10 +56,8 @@ class InputData:
 
         self.trainList = []
         self.trainIdList = []
-        self.trainUTM = np.zeros([2, self.trainNum], dtype=np.float32)
         for k in range(self.trainNum):
             self.trainList.append(self.id_alllist[self.training_inds[k][0]])
-            self.trainUTM[:, k] = self.utms_all[:, self.training_inds[k][0]]
             self.trainIdList.append(k)
 
         self.__cur_id = 0  # for training
@@ -76,48 +66,24 @@ class InputData:
         self.valNum = len(self.val_inds)
 
         self.valList = []
-        self.valUTM = np.zeros([2, self.valNum], dtype=np.float32)
         for k in range(self.valNum):
             self.valList.append(self.id_alllist[self.val_inds[k][0]])
-            self.valUTM[:, k] = self.utms_all[:, self.val_inds[k][0]]
         # cur validation index
         self.__cur_test_id = 0
 
     def next_batch_scan(self, batch_size, grd_noise=360, FOV=360):
         if self.__cur_test_id >= self.valNum:
             self.__cur_test_id = 0
-            return None, None, None, None, None
+            return None, None
         elif self.__cur_test_id + batch_size >= self.valNum:
             batch_size = self.valNum - self.__cur_test_id
 
         batch_polar_sat = np.zeros([batch_size, 128, 512, 3], dtype=np.float32)
-
-        batch_sat = np.zeros([batch_size, 256, 256, 3], dtype=np.float32)
-
         grd_width = int(FOV / 360 * 512)
-
         batch_grd = np.zeros([batch_size, 128, grd_width, 3], dtype=np.float32)
-        grd_shift = np.zeros([batch_size], dtype=np.int)
 
-        # the utm coordinates are used to define the positive sample and negative samples
-        batch_utm = np.zeros([batch_size, 2], dtype=np.float32)
-        batch_dis_utm = np.zeros([batch_size, batch_size, 1], dtype=np.float32)
         for i in range(batch_size):
             img_idx = self.__cur_test_id + i
-
-            # satellite
-            img = cv2.imread(self.valList[img_idx][1])
-            # img = cv2.resize(img, (self.satSize, self.satSize), interpolation=cv2.INTER_AREA)
-            if img is None or img.shape[0] != img.shape[1]:
-                print('InputData::next_pair_batch: read fail: %s, %d, ' % (self.valList[img_idx][1], i))
-                continue
-
-            img = img.astype(np.float32)
-
-            img[:, :, 0] -= 103.939  # Blue
-            img[:, :, 1] -= 116.779  # Green
-            img[:, :, 2] -= 123.6  # Red
-            batch_sat[i, :, :, :] = img
 
             # polar satellite
             img = cv2.imread(self.valList[img_idx][-1])
@@ -139,7 +105,7 @@ class InputData:
             if img is None or img.shape[0] * 4 != img.shape[1]:
                 print('InputData::next_pair_batch: read fail: %s, %d, ' % (self.valList[img_idx][0], i))
                 continue
-            img = cv2.resize(img, (self.panoCols, self.panoRows), interpolation=cv2.INTER_AREA)
+
             img = img.astype(np.float32)
 
             j = np.arange(0, 512)
@@ -152,22 +118,12 @@ class InputData:
             img_dup[:, :, 2] -= 123.6  # Red
             batch_grd[i, :, :, :] = img_dup
 
-            grd_shift[i] = random_shift
 
-            batch_utm[i, 0] = self.valUTM[0, img_idx]
-            batch_utm[i, 1] = self.valUTM[1, img_idx]
 
         self.__cur_test_id += batch_size
 
-        # compute the batch gps distance
-        for ih in range(batch_size):
-            for jh in range(batch_size):
-                batch_dis_utm[ih, jh, 0] = (batch_utm[ih, 0] - batch_utm[jh, 0]) * (
-                            batch_utm[ih, 0] - batch_utm[jh, 0]) + (batch_utm[ih, 1] - batch_utm[jh, 1]) * (
-                                                       batch_utm[ih, 1] - batch_utm[jh, 1])
 
-        return batch_polar_sat, batch_sat, batch_grd, batch_dis_utm, (
-            np.around(((512 - grd_shift) / 512 * 64) % 64)).astype(np.int)
+        return batch_polar_sat, batch_grd
 
     def next_pair_batch(self, batch_size, grd_noise=360, FOV=360):
         if self.__cur_id == 0:
@@ -180,15 +136,8 @@ class InputData:
 
         batch_polar_sat = np.zeros([batch_size, 128, 512, 3], dtype=np.float32)
 
-        batch_sat = np.zeros([batch_size, 256, 256, 3], dtype=np.float32)
-
         grd_width = int(FOV / 360 * 512)
         batch_grd = np.zeros([batch_size, 128, grd_width, 3], dtype=np.float32)
-        grd_shift = np.zeros([batch_size, ], dtype=np.int)
-
-        # # the utm coordinates are used to define the positive sample and negative samples
-        batch_utm = np.zeros([batch_size, 2], dtype=np.float32)
-        batch_dis_utm = np.zeros([batch_size, batch_size, 1], dtype=np.float32)
 
         i = 0
         batch_idx = 0
@@ -198,21 +147,6 @@ class InputData:
 
             img_idx = self.trainIdList[self.__cur_id + i]
             i += 1
-
-            # satellite
-            img = cv2.imread(self.trainList[img_idx][1])
-            if img is None:
-                print('InputData::next_pair_batch: read fail: %s, %d, ' % (self.trainList[img_idx][1], i))
-                continue
-
-            img = img.astype(np.float32)
-
-            # normalize it to -1 --- 1
-            img[:, :, 0] -= 103.939  # Blue
-            img[:, :, 1] -= 116.779  # Green
-            img[:, :, 2] -= 123.6  # Red
-
-            batch_sat[batch_idx, :, :, :] = img
 
             # polar satellite
             img = cv2.imread(self.trainList[img_idx][-1])
@@ -235,7 +169,7 @@ class InputData:
             if img is None or img.shape[0] * 4 != img.shape[1]:
                 print('InputData::next_pair_batch: read fail: %s, %d, ' % (self.trainList[img_idx][0], i))
                 continue
-            img = cv2.resize(img, (512, 128), interpolation=cv2.INTER_AREA)
+
             img = img.astype(np.float32)
 
             j = np.arange(0, 512)
@@ -247,23 +181,12 @@ class InputData:
             img_dup[:, :, 2] -= 123.6  # Red
             batch_grd[batch_idx, :, :, :] = img_dup
 
-            grd_shift[batch_idx] = random_shift
-            batch_utm[batch_idx, 0] = self.trainUTM[0, img_idx]
-            batch_utm[batch_idx, 1] = self.trainUTM[1, img_idx]
 
             batch_idx += 1
 
-        # compute the batch gps distance
-        for ih in range(batch_size):
-            for jh in range(batch_size):
-                batch_dis_utm[ih, jh, 0] = (batch_utm[ih, 0] - batch_utm[jh, 0]) * (
-                            batch_utm[ih, 0] - batch_utm[jh, 0]) + (batch_utm[ih, 1] - batch_utm[jh, 1]) * (
-                                                       batch_utm[ih, 1] - batch_utm[jh, 1])
-
         self.__cur_id += i
-        # return batch_sat, batch_grd, batch_dis_utm
-        return batch_polar_sat, batch_sat, batch_grd, batch_dis_utm, (
-            np.around(((512 - grd_shift) / 512 * 64) % 64)).astype(np.int)
+
+        return batch_polar_sat, batch_grd
 
     def get_dataset_size(self):
         return self.trainNum
@@ -279,4 +202,4 @@ class InputData:
 
 if __name__ == '__main__':
     input_data = InputData()
-    batch_sat, batch_grd, batch_utm = input_data.next_batch_scan(12)
+    batch_sat, batch_grd = input_data.next_batch_scan(12)
